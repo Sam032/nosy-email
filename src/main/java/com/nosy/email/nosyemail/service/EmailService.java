74a57e44edc25f3f2a545c20712a72c90ad26358
Oktay <oktay.alizada@rectasolutions.com>
2019-06-02 01:57:31 +0200

    Add EmailServiceTest for send method success

---
+++ src/main/java/com/nosy/email/nosyemail/service/EmailService.java
@@ -0,0 +1,81 @@
+package com.nosy.email.nosyemail.service;
+
+import com.nosy.email.nosyemail.model.ReadyEmail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+@Component
+public class EmailService {
+    private static final String DEFAULT_FROM_PROVIDER="DEFAULT";
+    private Logger logger = LoggerFactory.getLogger(EmailService.class);
+
+  @Value("${nosy.email.default.username}")
+  private String emailDefaultUsername;
+
+    @Value("${nosy.email.default.password}")
+    private String emailDefaultPassword;
+
+    public void send(ReadyEmail readyEmail, JavaMailSenderImpl javaMailSender){
+        if (readyEmail.getEmailTemplate().getEmailFromProvider().equals(DEFAULT_FROM_PROVIDER)) {
+            javaMailSender.setUsername(emailDefaultUsername);
+            javaMailSender.setPassword(emailDefaultPassword);
+            readyEmail.getEmailTemplate().setFromAddress(emailDefaultUsername);
+        }
+        else{
+            javaMailSender.setUsername(readyEmail.getEmailProviderProperties().getUsername());
+            javaMailSender.setPassword(readyEmail.getEmailProviderProperties().getPassword());
+        }
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message);
+        try {
+
+            if (readyEmail.getEmailTemplate().getFromAddress() != null) {
+                mimeMessageHelper.setFrom(readyEmail.getEmailTemplate().getFromAddress());
+            }
+            mimeMessageHelper.setSubject(readyEmail.getEmailTemplate().getSubject());
+            mimeMessageHelper.setText(
+                    readyEmail.getEmailTemplate().getFromAddress()
+                            + ":   "
+                            + readyEmail.getEmailTemplate().getText(),
+                    true);
+            readyEmail
+                    .getEmailTemplate()
+                    .getEmailTemplateTo()
+                    .forEach(
+                            emailTo -> {
+                                try {
+                                    mimeMessageHelper.addTo(emailTo);
+                                } catch (MessagingException e) {
+                                    logger.error(e.getMessage());
+                                }
+                            });
+
+            if (!readyEmail.getEmailTemplate().getEmailTemplateCc().isEmpty()) {
+                readyEmail
+                        .getEmailTemplate()
+                        .getEmailTemplateCc()
+                        .forEach(
+                                emailCc -> {
+                                    try {
+                                        mimeMessageHelper.addCc(emailCc);
+
+                                    } catch (MessagingException e) {
+                                        logger.error(e.getMessage());
+                                    }
+                                });
+            }
+
+            javaMailSender.send(message);
+
+        } catch (MessagingException messageException) {
+            logger.error(messageException.getMessage());
+            throw new RuntimeException(messageException);
+        }
+    }
+}
---
+++ src/test/java/com/nosy/email/nosyemail/service/EmailServiceTest.java
@@ -0,0 +1,66 @@
+package com.nosy.email.nosyemail.service;
+
+import com.nosy.email.nosyemail.model.EmailProviderProperties;
+import com.nosy.email.nosyemail.model.EmailTemplate;
+import com.nosy.email.nosyemail.model.ReadyEmail;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import javax.mail.internet.MimeMessage;
+import java.util.HashSet;
+import java.util.Set;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class EmailServiceTest {
+    @InjectMocks
+    private EmailService emailService;
+    private ReadyEmail readyEmail;
+
+    @Before
+    public void setUp(){
+        readyEmail=new ReadyEmail();
+        EmailTemplate emailTemplate=new EmailTemplate();
+        emailTemplate.setFromAddress("test@nosy.tech");
+        emailTemplate.setEmailFromProvider("DEFAULT");
+        emailTemplate.setEmailTemplateId("emailTemplateId");
+        Set<String> emailTemplateToSet=new HashSet<>();
+        String emailTemplateTo="nosy@email.to";
+        emailTemplateToSet.add(emailTemplateTo);
+        Set<String> emailTemplateCcSet=new HashSet<>();
+        String emailTemplateCc="nosy@email.to";
+        emailTemplateToSet.add(emailTemplateCc);
+
+        emailTemplate.setEmailTemplateCc(emailTemplateCcSet);
+        emailTemplate.setEmailTemplateTo(emailTemplateToSet);
+        emailTemplate.setPriority(1);
+        emailTemplate.setRetryPeriod(1);
+        emailTemplate.setRetryTimes(1);
+        emailTemplate.setSubject("subject");
+        emailTemplate.setText("text");
+        readyEmail.setEmailTemplate(emailTemplate);
+
+
+    }
+
+
+
+  @Test(expected = Test.None.class)
+  public void send() {
+      EmailProviderProperties emailProviderProperties=new EmailProviderProperties();
+      emailProviderProperties.setPassword("dadad");
+      emailProviderProperties.setUsername("dadasdas");
+      JavaMailSenderImpl javaMailSender=mock(JavaMailSenderImpl.class);
+      MimeMessage mimeMessage=mock(MimeMessage.class);
+      when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+      doNothing().when(javaMailSender).send(mimeMessage);
+      emailService.send(readyEmail,javaMailSender);
+
+
+  }
+}
